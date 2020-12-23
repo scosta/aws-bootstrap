e15179851512a2e5a1e004f5f3c30ea95d32da9b
scosta <stevencosta@gmail.com>
2020-12-23 15:21:52 -0500

    show instance endpoint on build

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -15,3 +15,10 @@ aws cloudformation deploy \
 --template-file main.yml \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?Name=='InstanceEndpoint'].Value" 
+fi
